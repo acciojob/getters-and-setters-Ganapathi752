49e13051432c68f89f90953aeaf0e18ac5a0b69e
Ganapathi752 <gana.ganapathi2003@gmail.com>
2025-11-02 20:31:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
 //complete this code
-class Person{
-    constructor(name, age){
-        this._name = name
-        this._age = age
-    }
+class Person {
+	constructor(name, age){
+		this._name = name
+		this._age = age
+	}
 
-    get name(){
-        return this._name
-    }
+	get name(){
+		return this._name
+	}
 
-    get age(){
-        return this._age
-    }
+	get age(){
+		return this._age
+	}
 
-    set age(newAge){
-        this._age = newAge
-    }
+	set age(newAge){
+		this._age = newAge
+	}
 }
 
-class Student extends Person{
-    constructor(name){
-        super(name)
-    }
+class Student extends Person {
+	constructor(name){
+		super(name)
+	}
 
-    study(){
-        console.log(`${this.name} is studying`)
-    }
+	study(){
+		console.log(`${this.name} is studying`)
+	}
 }
 
-class Teacher extends Person{
-    constructor(name){
-        super(name)
-    }
+class Teacher extends Person {
+	constructor(name){
+		super(name)
+	}
 
-    teach(){
-        console.log(`${this.name} is teaching`)
-    }
+	teach(){
+		console.log(`${this.name} is teaching`)
+	}
 }
+
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
